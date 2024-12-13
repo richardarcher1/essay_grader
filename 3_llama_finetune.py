a7b24d325d94516ec005b54bcfc2f6c1a0e7dae2
richardarcher1 <richard.archer@yale.edu>
2024-12-12 20:56:39 -0500

    2024_12_12_20_56_commit

--- 3_llama_finetune.py
+++ 3_llama_finetune.py
@@ -60,7 +60,7 @@ def main():
     print("TRY BIGGER GPU")
 
     nf4_config = BitsAndBytesConfig(
-        load_in_8bit=True, # NOTE WAS 4 BIT
+        load_in_4bit=True, # NOTE WAS 4 BIT
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.float16,  # Match input dtype
     )
